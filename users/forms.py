c7b4a5f882dafa32031105bec5e14c681b80e718
dhruvdeshpande02 <101721546+dhruvdeshpande02@users.noreply.github.com>
2024-03-18 16:51:38 +0530

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -5,12 +5,21 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from django.contrib.auth import authenticate  # Import the authenticate function
 from django import forms
-from .models import CustomUser, Student
+from .models import CustomUser, Student,Coordinator
 class CustomUserForm(UserCreationForm):
-  class Meta:
-    model = CustomUser
-    fields = ['email']
-              
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'user_type']  # Include required fields directly
+
+    def __init__(self, *args, view_name=None, **kwargs):
+        super(CustomUserForm, self).__init__(*args, **kwargs)
+        self.fields['email'].label = 'Email Address'  # Customize email label
+
+        if view_name in ['register_student']:
+            self.fields['user_type'].widget.attrs.update({'disabled': True})
+                    # ... other field customizations for user_type if needed ...
+                
+                  
 
 class StudentForm(forms.ModelForm):
   class Meta:
@@ -18,5 +27,24 @@ class StudentForm(forms.ModelForm):
         fields = '__all__'  # Include all fields by default
         exclude = ['user','AVG_TILL_SEM_cgpa','AVG_TILL_SEM_percentage']  
 
+class CoordinatorForm(forms.ModelForm):
+   class Meta:
+         model =Coordinator
+         fields = '__all__'
+         exclude = ['user']
 
+from django import forms
+from django.contrib.auth import authenticate
 
+class LoginForm(forms.Form):
+  email = forms.EmailField(max_length=254)
+  password = forms.CharField(widget=forms.PasswordInput)
+
+  def clean(self):
+    cleaned_data = super().clean()
+    email = cleaned_data.get('email')
+    password = cleaned_data.get('password')
+    user = authenticate(email=email, password=password)
+    if not user:
+      raise forms.ValidationError('Invalid email or password')
+    return cleaned_data
