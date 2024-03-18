1cbc36c87066b95a74947f35ff1e8de5de27dc90
dhruvdeshpande02 <101721546+dhruvdeshpande02@users.noreply.github.com>
2024-03-18 16:54:24 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,26 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('all-students/', views.all_students, name='all_students'),
+    path('register-student/', views.register_student, name='register-student'),
+    path('register-coordinator/', views.register_coordinator, name='register-coordinator'),
+
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+
     
-    path('all-drives/',views.drives, name ='all-drives'),
-    path('drive/<str:pk>/',views.drive, name='drive'),
-    path('download-drive-pdf/<int:pk>/', views.download_drive_pdf, name='download_drive_pdf'),
-    path('create-drive/',views.createDrive,name='create-drive'),
-    path('update-drive/<str:pk>/',views.updateDrive, name='update-drive'),
-    path('delete-drive/<str:pk>/', views.delete_Drive, name='delete-drive'),
-    path('activities/', views.activities, name='activities'),
-    path('activity/<str:pk>/',views.activity, name='activity'),
-    path('download-activity-pdf/<int:pk>/', views.download_activity_pdf, name='download_activity_pdf'),
-    path('create-activity/',views.createActivity,name='create-activity'),
-    path('update-activity/<str:pk>/',views.updateActivity, name='update-activity'),
-    path('delete-activity/<str:pk>/', views.delete_Activity, name='delete-activity'),
-    path('view/',views.my_view, name='view'),
-    path('sidebar/',views.sidebar, name='sidebar'),
 
-    path('dashboard/',views.dashboard, name='dashboard'),
-
-
-
-
-
-    ]
+]
